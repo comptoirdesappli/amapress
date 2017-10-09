0ee3f88525d6beb60120e53e96c467216cea13d5
Guillaume <sharevb@gmail.com>
2017-10-09 08:17:41 +0200

    Variable globale contenant la liste des shortcodes (pour les tests)

--- shortcodes/utils.php
+++ shortcodes/utils.php
@@ -3,10 +3,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+global $all_amapress_shortcodes;
 /**
  * @param string $tag
  * @param callable $func
  */
 function amapress_register_shortcode( $tag, $func ) {
+	global $all_amapress_shortcodes;
+	if ( empty( $all_amapress_shortcodes ) ) {
+		$all_amapress_shortcodes = [];
+	}
+	$all_amapress_shortcodes[ $tag ] = $func;
 	add_shortcode( $tag, $func );
 }
