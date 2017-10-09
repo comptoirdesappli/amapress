71962a347fa9140046dca1382ca96bf8949a93ef
Guillaume <sharevb@gmail.com>
2017-10-09 08:21:05 +0200

    Mise en place phpunit, bootstrap

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -1,18 +1,32 @@
 <?php
 
-/**
- * PHPUnit tests bootstrap.
- */
+$wp_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-/**
- * The Composer-generated autoloader.
- */
-require_once( dirname( __FILE__ ) . '/../../../vendor/autoload.php' );
+if ( ! $wp_tests_dir ) {
+	echo( '$_ENV["WP_TESTS_DIR"] is not set.' . PHP_EOL );
+	exit( 1 );
+}
+require_once $wp_tests_dir . '/includes/functions.php';
 
-$_ENV['WP_TESTS_DIR'] = '/tmp/wp2/wordpress-tests-lib';
+function _manually_load_environment() {
+// Add your theme
+	switch_theme( 'twentysixteen' );
 
-$loader = WPPPB_Loader::instance();
-$loader->add_plugin( 'amapress.php' );
-$loader->load_wordpress();
+// Update array with plugins to include ...
+	$plugins_to_active = array(
+		'amapress/amapress.php'
+	);
 
-// EOF
+
+	foreach ( $plugins_to_active as $plugin ) {
+		require dirname( __FILE__ ) . '/../../../../' . $plugin;
+	}
+
+	update_option( 'active_plugins', $plugins_to_active );
+}
+
+tests_add_filter( 'muplugins_loaded', '_manually_load_environment' );
+
+require $wp_tests_dir . '/includes/bootstrap.php';
+
+require_once dirname( __FILE__ ) . '/class.amapress.unit.testcase.php';
