e1f885dd12fe73029fb4f0aab7d3a5a0f17aa27f
Guillaume <sharevb@gmail.com>
2017-10-12 00:17:58 +0200

    Affichage du code quantité dans la liste émargement

--- entities/contrat_adhesion/classes.php
+++ entities/contrat_adhesion/classes.php
@@ -142,23 +142,23 @@ class AmapressAdhesion extends TitanEntity {
 			/** @var AmapressContrat_quantite $q */
 			return $q->getCode();
 		}, $this->getContrat_quantites() );
-		$quants = array_map( function ( $q ) {
-			/** @var AmapressContrat_quantite $q */
-			return $q->getQuantite();
-		}, $this->getContrat_quantites() );
-		$titles = array_map( function ( $q ) {
-			/** @var AmapressContrat_quantite $q */
-			$c = $q->getCode();
-
-			return ! empty( $c ) ? $c : $q->getTitle();
-		}, $this->getContrat_quantites() );
-		if ( count( array_unique( $quants ) ) == count( $this->getContrat_quantites() ) ) {
-			return implode( ',', $quants );
-		} else if ( count( array_unique( $codes ) ) == count( $this->getContrat_quantites() ) ) {
+//		$quants = array_map( function ( $q ) {
+//			/** @var AmapressContrat_quantite $q */
+//			return $q->getQuantite();
+//		}, $this->getContrat_quantites() );
+//		$titles = array_map( function ( $q ) {
+//			/** @var AmapressContrat_quantite $q */
+//			$c = $q->getCode();
+//
+//			return ! empty( $c ) ? $c : $q->getTitle();
+//		}, $this->getContrat_quantites() );
+//		if ( count( array_unique( $quants ) ) == count( $this->getContrat_quantites() ) ) {
+//			return implode( ',', $quants );
+//		} else if ( count( array_unique( $codes ) ) == count( $this->getContrat_quantites() ) ) {
 			return implode( ',', $codes );
-		} else {
-			return implode( ',', $titles );
-		}
+//		} else {
+//			return implode( ',', $titles );
+//		}
 	}
 
 	public function getContrat_quantites_IDs() {
