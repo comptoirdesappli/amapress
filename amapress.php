fb318781e259a584c77cbe492b4d18545753f39f
Guillaume <sharevb@gmail.com>
2017-10-21 02:10:38 +0200

    Présentation Web : n'afficher le thumbnail (supposé un logo) que si la taille du thumb demandé est carré

--- amapress.php
+++ amapress.php
@@ -435,6 +435,108 @@ function amapress_get_avatar_meta_name() {
 	return $wpdb->get_blog_prefix( $blog_id ) . 'user_avatar';
 }
 
+/**
+ * Get size information for all currently-registered image sizes.
+ *
+ * @global $_wp_additional_image_sizes
+ * @uses   get_intermediate_image_sizes()
+ * @return array $sizes Data for all currently-registered image sizes.
+ */
+function get_image_sizes() {
+	global $_wp_additional_image_sizes;
+
+	$sizes = array();
+
+	foreach ( get_intermediate_image_sizes() as $_size ) {
+		if ( in_array( $_size, array( 'thumbnail', 'medium', 'medium_large', 'large' ) ) ) {
+			$sizes[ $_size ]['width']  = get_option( "{$_size}_size_w" );
+			$sizes[ $_size ]['height'] = get_option( "{$_size}_size_h" );
+			$sizes[ $_size ]['crop']   = (bool) get_option( "{$_size}_crop" );
+		} elseif ( isset( $_wp_additional_image_sizes[ $_size ] ) ) {
+			$sizes[ $_size ] = array(
+				'width'  => $_wp_additional_image_sizes[ $_size ]['width'],
+				'height' => $_wp_additional_image_sizes[ $_size ]['height'],
+				'crop'   => $_wp_additional_image_sizes[ $_size ]['crop'],
+			);
+		}
+	}
+
+	return $sizes;
+}
+
+/**
+ * Get size information for a specific image size.
+ *
+ * @uses   get_image_sizes()
+ *
+ * @param  string $size The image size for which to retrieve data.
+ *
+ * @return bool|array $size Size data about an image size or false if the size doesn't exist.
+ */
+function get_image_size( $size ) {
+	$sizes = get_image_sizes();
+
+	if ( isset( $sizes[ $size ] ) ) {
+		return $sizes[ $size ];
+	}
+
+	return false;
+}
+
+/**
+ * Get the width of a specific image size.
+ *
+ * @uses   get_image_size()
+ *
+ * @param  string $size The image size for which to retrieve data.
+ *
+ * @return bool|string $size Width of an image size or false if the size doesn't exist.
+ */
+function get_image_width( $size ) {
+	if ( ! $size = get_image_size( $size ) ) {
+		return false;
+	}
+
+	if ( isset( $size['width'] ) ) {
+		return $size['width'];
+	}
+
+	return false;
+}
+
+/**
+ * Get the height of a specific image size.
+ *
+ * @uses   get_image_size()
+ *
+ * @param  string $size The image size for which to retrieve data.
+ *
+ * @return bool|string $size Height of an image size or false if the size doesn't exist.
+ */
+function get_image_height( $size ) {
+	if ( ! $size = get_image_size( $size ) ) {
+		return false;
+	}
+
+	if ( isset( $size['height'] ) ) {
+		return $size['height'];
+	}
+
+	return false;
+}
+
+add_filter( 'post_thumbnail_html', function ( $html, $post_id, $post_thumbnail_id, $size, $attr ) {
+	$post_type = get_post_type( $post_id );
+
+	if ( AmapressContrat::INTERNAL_POST_TYPE == $post_type ) {
+		if ( get_image_height( $size ) !== get_image_width( $size ) ) {
+			return '';
+		}
+	}
+
+	return $html;
+}, 10, 5 );
+
 add_action( 'updated_postmeta', 'amapress_updated_postmeta', 10, 4 );
 function amapress_updated_postmeta( $meta_id, $object_id, $meta_key, $meta_value ) {
 	if ( $meta_key == '_thumbnail_id' && $meta_value ) {
