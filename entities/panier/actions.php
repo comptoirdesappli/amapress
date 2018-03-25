cd61e8df4fd7c4c6df4a380e3b132586e7671b49
Guillaume <sharevb@gmail.com>
2018-03-26 00:56:43 +0200

    Harmonisation date limite cession panier

--- entities/panier/actions.php
+++ entities/panier/actions.php
@@ -99,8 +99,8 @@ function amapress_echanger_panier( $panier_ids, $user_id = null, $message = null
 	foreach ( $panier_ids as $panier_id ) {
 		$panier = AmapressPanier::getBy( $panier_id );
 
-		//TODO : parametre pour date limite
-		if ( Amapress::hour_of_day( $panier->getRealDate(), 16 ) < amapress_time() ) {
+		$can_subscribe = Amapress::start_of_day( $panier->getRealDate() ) >= Amapress::start_of_day( amapress_time() );
+		if ( ! $can_subscribe ) {
 			return 'too_late';
 		}
 
