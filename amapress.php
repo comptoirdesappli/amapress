9a629fedb5e2c11a3773790f101c5307c2841823
Guillaume <sharevb@gmail.com>
2018-07-09 08:49:46 +0200

    Merge - correction bug de droits (edit_others_post et autres)

--- amapress.php
+++ amapress.php
@@ -6,7 +6,7 @@
 Plugin Name: Amapress
 Plugin URI: http://amapress.fr/
 Description: 
-Version: 0.45.15
+Version: 0.45.20
 Requires PHP: 5.6
 Author: ShareVB
 Author URI: http://amapress.fr/
@@ -45,8 +45,8 @@ define( 'AMAPRESS__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'AMAPRESS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'AMAPRESS__PLUGIN_FILE', __FILE__ );
 define( 'AMAPRESS_DELETE_LIMIT', 100000 );
-define( 'AMAPRESS_DB_VERSION', 70 );
-define( 'AMAPRESS_VERSION', '0.45.15' );
+define( 'AMAPRESS_DB_VERSION', 71 );
+define( 'AMAPRESS_VERSION', '0.45.20' );
 //remove_role('responable_amap');
 
 require_once AMAPRESS__PLUGIN_DIR . 'vendor/autoload.php';
