d5a63be93e7cd36d9af95ad5a3dcf2268d213f30
Guillaume <sharevb@gmail.com>
2019-03-17 21:26:02 +0100

    Ical : ajout de X-WR-TIMEZONE

--- entities/ical.php
+++ entities/ical.php
@@ -4,6 +4,41 @@
  * Event ICAL feed
  */
 class Amapress_Agenda_ICAL_Export {
+	/**
+	 * Finds valid timezone for timezone_string setting in wp
+	 *
+	 * @return string Valid timezone
+	 *
+	 * @see: https://www.skyverge.com/blog/down-the-rabbit-hole-wordpress-and-timezones/
+	 */
+	private static function getTimezoneString() {
+		// if site timezone string exists, return it
+		if ( $timezone = get_option( 'timezone_string' ) ) {
+			return $timezone;
+		}
+		// get UTC offset, if it isn't set then return UTC
+		if ( 0 === ( $utc_offset = get_option( 'gmt_offset', 0 ) ) ) {
+			return 'UTC';
+		}
+		// adjust UTC offset from hours to seconds
+		$utc_offset *= 3600;
+		// attempt to guess the timezone string from the UTC offset
+		if ( $timezone = timezone_name_from_abbr( '', $utc_offset, 0 ) ) {
+			return $timezone;
+		}
+		// last try, guess timezone string manually
+		$is_dst = date( 'I' );
+		foreach ( timezone_abbreviations_list() as $abbr ) {
+			foreach ( $abbr as $city ) {
+				if ( $city['dst'] == $is_dst && $city['offset'] == $utc_offset ) {
+					return $city['timezone_id'];
+				}
+			}
+		}
+
+		// fallback to UTC
+		return 'UTC';
+	}
 
 	private static function ical_split( $preamble, $value ) {
 		$value        = trim( $value );
@@ -98,6 +133,7 @@ class Amapress_Agenda_ICAL_Export {
 		echo "PRODID:-//Amapress//NONSGML Events//FR\n";
 		echo "CALSCALE:GREGORIAN\n";
 		echo "X-WR-CALNAME:" . self::ical_split( 'X-WR-CALNAME:', get_bloginfo( 'name' ) ) . " - Agenda\n";
+		echo "X-WR-TIMEZONE:" . self::ical_split( 'X-WR-TIMEZONE:', self::getTimezoneString() ) . "\n";
 
 		$offset = get_option( 'gmt_offset' ) * 3600;
 		foreach ( $events as $event ) {
