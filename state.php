73d675feed4a12692673ef01ec5520dfb6560be8
Guillaume <sharevb@gmail.com>
2019-07-12 08:31:44 +0200

    Etat AMapress : collapsible heightStyle content

--- state.php
+++ state.php
@@ -1634,6 +1634,7 @@ function amapress_echo_and_check_amapress_state_page() {
 	echo '<script type="text/javascript">
 	jQuery(document).ready(function($) {
 		$( "#amps-state-accordion" ).accordion({
+			heightStyle: "content",
 			collapsible: true,
 			active : "none"
 		});
