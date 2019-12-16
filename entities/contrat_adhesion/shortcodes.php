1609f615d7786a34d17ca85f825ff14eb707c902
sharevb <sharevb@gmail.com>
2019-12-16 01:17:19 +0100

    Assistant inscription : option show_due_amounts / Récapitulatif des sommes dues

--- entities/contrat_adhesion/shortcodes.php
+++ entities/contrat_adhesion/shortcodes.php
@@ -250,29 +250,30 @@ function amapress_self_inscription( $atts, $content = null, $tag ) {
 			'paniers_modulables_editor_height' => 350,
 			'edit_names'                       => 'true',
 			'allow_remove_coadhs'              => 'false',
-			'contact_referents'                => 'true',
-			'show_adherents_infos'             => 'true',
+			'contact_referents'             => 'true',
+			'show_adherents_infos'          => 'true',
 //			'allow_edit_inscriptions'          => 'true',
-			'allow_coadherents_access'         => 'true',
-			'allow_coadherents_inscription'    => 'true',
-			'allow_coadherents_adhesion'       => 'true',
-			'show_coadherents_address'         => 'false',
-			'contrat_print_button_text'        => 'Imprimer',
-			'adhesion_print_button_text'       => 'Imprimer',
-			'only_contrats'                    => '',
-			'shorturl'                         => '',
-			'adhesion_shift_weeks'             => 0,
-			'before_close_hours'               => 24,
-			'max_coadherents'                  => 3,
-			'email'                            => get_option( 'admin_email' ),
+			'allow_coadherents_access'      => 'true',
+			'allow_coadherents_inscription' => 'true',
+			'allow_coadherents_adhesion'    => 'true',
+			'show_coadherents_address'      => 'false',
+			'contrat_print_button_text'     => 'Imprimer',
+			'adhesion_print_button_text'    => 'Imprimer',
+			'only_contrats'                 => '',
+			'shorturl'                      => '',
+			'show_due_amounts'              => 'false',
+			'adhesion_shift_weeks'          => 0,
+			'before_close_hours'            => 24,
+			'max_coadherents'               => 3,
+			'email'                         => get_option( 'admin_email' ),
 		]
 		, $atts );
 
-	$contrat_print_button_text  = $atts['contrat_print_button_text'];
-	$adhesion_print_button_text = $atts['adhesion_print_button_text'];
-	$for_logged                 = Amapress::toBool( $atts['for_logged'] );
-	$ret                        = '';
-	$admin_mode                 = Amapress::toBool( $atts['admin_mode'] );
+	$contrat_print_button_text     = $atts['contrat_print_button_text'];
+	$adhesion_print_button_text    = $atts['adhesion_print_button_text'];
+	$for_logged                    = Amapress::toBool( $atts['for_logged'] );
+	$ret                           = '';
+	$admin_mode                    = Amapress::toBool( $atts['admin_mode'] );
 	if ( $admin_mode && ! is_admin() ) {
 		wp_die( 'admin_mode ne peut pas être utilisé directement' );
 	}
@@ -286,6 +287,7 @@ function amapress_self_inscription( $atts, $content = null, $tag ) {
 	$show_adherents_infos          = Amapress::toBool( $atts['show_adherents_infos'] );
 	$track_no_renews               = Amapress::toBool( $atts['track_no_renews'] );
 	$show_coadherents_address      = Amapress::toBool( $atts['show_coadherents_address'] );
+	$show_due_amounts              = Amapress::toBool( $atts['show_due_amounts'] );
 //	$allow_edit_inscriptions       = Amapress::toBool( $atts['allow_edit_inscriptions'] );
 	$notify_email = $atts['notify_email'];
 	if ( ! $allow_coadherents_inscription ) {
@@ -1433,6 +1435,11 @@ Vous pouvez configurer l\'email envoyé en fin de chaque inscription <a target="
 				}
 			}
 		} else if ( ! empty( $adhs ) ) {
+			if ( $show_due_amounts ) {
+				echo '<p>' . Amapress::makeButtonLink( add_query_arg( [
+						'step' => 'details_all_paiements',
+					] ), 'Récapitulatif des sommes dues', true, true, 'btn btn-default' ) . '</p>';
+			}
 			if ( ! $admin_mode ) {
 				echo '<p>Vos contrats :</p>';
 			} else {
@@ -1440,7 +1447,7 @@ Vous pouvez configurer l\'email envoyé en fin de chaque inscription <a target="
 			}
 			echo '<ul style="list-style-type: disc">';
 			foreach ( $adhs as $adh ) {
-				$print_contrat = '';
+				$print_contrat        = '';
 				if ( ! empty( $adh->getContrat_instance()->getContratModelDocFileName() ) ) {
 					$print_contrat = Amapress::makeButtonLink(
 						add_query_arg( [
@@ -1476,7 +1483,7 @@ Vous pouvez configurer l\'email envoyé en fin de chaque inscription <a target="
 							'step'       => 'inscr_contrat_date_lieu',
 							'contrat_id' => $adh->getContrat_instanceId()
 						] );
-						$edit_contrat    = '<br/>
+						$edit_contrat = '<br/>
 <form style="display: inline-block; margin-left: 5px" method="get" action="' . esc_attr( $inscription_url ) . '">
 <input type="hidden" name="key" value="' . $key . '" />
 <input type="hidden" name="step" value="inscr_contrat_date_lieu" />
@@ -1773,12 +1780,93 @@ Vous pouvez configurer l\'email envoyé en fin de chaque inscription <a target="
             <input type="submit" value="Valider" class="btn btn-default btn-assist-inscr"/>
         </form>
 		<?php
-	} else if ( 'details' == $step ) {
-		if ( empty( $_GET['contrat_id'] ) || empty( $_GET['user_id'] ) ) {
+	} else if ( 'details_all_paiements' == $step ) {
+		if ( ! $show_due_amounts ) {
 			wp_die( $invalid_access_message );
 		}
+		if ( $for_logged && amapress_is_user_logged_in() ) {
+			$user_id = wp_get_current_user()->ID;
+		} else {
+			if ( empty( $_REQUEST['user_id'] ) ) {
+				wp_die( $invalid_access_message );
+			}
+			$user_id = intval( $_REQUEST['user_id'] );
+		}
+
+		Amapress::setFilterForReferent( false );
+		$adhs = AmapressAdhesion::getUserActiveAdhesions( $user_id, null, null, false, true );
+		Amapress::setFilterForReferent( true );
+
+		echo '<h4>Récapitulatif des sommes dues</h4>';
+		$columns   = [];
+		$columns[] = array(
+			'title' => 'Producteur',
+			'data'  => array(
+				'_'    => 'prod',
+				'sort' => 'prod',
+			)
+		);
+		$columns[] = array(
+			'title' => 'Total',
+			'data'  => array(
+				'_'    => 'total_d',
+				'sort' => 'total',
+			)
+		);
+		$columns[] = array(
+			'title' => 'Option paiements',
+			'data'  => array(
+				'_'    => 'opt_pmts',
+				'sort' => 'opt_pmts',
+			)
+		);
+		$columns[] = array(
+			'title' => 'Info',
+			'data'  => array(
+				'_'    => 'info',
+				'sort' => 'info',
+			)
+		);
+
+
+		$data = [];
+		foreach ( $adhs as $adh ) {
+			$row             = [];
+			$row['prod']     = $adh->getContrat_instance()->getModel()->getTitle()
+			                   . '<br />'
+			                   . '<em>' . $adh->getContrat_instance()->getModel()->getProducteur()->getTitle() . '</em>';
+			$row['opt_pmts'] = $adh->getProperty( 'option_paiements' );
+			$info            = 'Ordre: ' . $adh->getProperty( 'paiements_ordre' );
+			$info            .= ! empty( $adh->getProperty( 'paiements_mention' ) ) ? '<br/>' . $adh->getProperty( 'paiements_mention' ) : '';
+			$row['info']     = $info;
+			$row['total_d']  = Amapress::formatPrice( $adh->getTotalAmount(), true );
+			$row['total']    = $adh->getTotalAmount();
+			$data[]          = $row;
+		}
+		echo amapress_get_datatable( 'details_all_paiements', $columns, $data,
+			array(
+				'paging'    => false,
+				'searching' => false,
+			),
+			array(
+				Amapress::DATATABLES_EXPORT_EXCEL,
+				Amapress::DATATABLES_EXPORT_PRINT
+			) );
+	} else if ( 'details' == $step ) {
+		if ( empty( $_GET['contrat_id'] ) ) {
+			wp_die( $invalid_access_message );
+		}
+		if ( $for_logged && amapress_is_user_logged_in() ) {
+			$user_id = wp_get_current_user()->ID;
+		} else {
+			if ( empty( $_REQUEST['user_id'] ) ) {
+				wp_die( $invalid_access_message );
+			}
+			$user_id = intval( $_REQUEST['user_id'] );
+		}
+
 		$adh = AmapressAdhesion::getBy( intval( $_GET['contrat_id'] ) );
-		if ( $adh->getAdherentId() != intval( $_GET['user_id'] ) ) {
+		if ( $adh->getAdherentId() != intval( $user_id ) ) {
 			wp_die( $invalid_access_message );
 		}
 		if ( ! empty( $_GET['cancel_inscr_id'] ) ) {
