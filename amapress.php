5ed841724a801831e68447c00d3906396a4cec30
sharevb <sharevb@gmail.com>
2020-03-09 09:02:38 +0100

    feat: Report vers distribution non existante

--- amapress.php
+++ amapress.php
@@ -51,7 +51,7 @@ define( 'AMAPRESS__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'AMAPRESS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'AMAPRESS__PLUGIN_FILE', __FILE__ );
 define( 'AMAPRESS_DELETE_LIMIT', 100000 );
-define( 'AMAPRESS_DB_VERSION', 105 );
+define( 'AMAPRESS_DB_VERSION', 106 );
 define( 'AMAPRESS_VERSION', '0.92.105' );
 //remove_role('responable_amap');
 
