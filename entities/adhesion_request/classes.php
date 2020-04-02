23f654539338386daf8db23eab625cc502683fde
sharevb <sharevb@gmail.com>
2020-04-02 09:03:04 +0200

    feat(Demandes d'adhésions): ajout email et contrats + refactoring, class et clean

--- entities/adhesion_request/classes.php
+++ entities/adhesion_request/classes.php
@@ -12,4 +12,77 @@ class AmapressAdhesionRequest extends TitanEntity {
 		parent::__construct( $post_id );
 	}
 
+	/** @return AmapressAdhesionRequest[] */
+	public static function getAllToConfirm() {
+		return self::getAll( 'to_confirm' );
+	}
+
+	/** @return AmapressAdhesionRequest[] */
+	public static function getAll( $status = null ) {
+		$key = "amapress_mlgrp_all_list_$status";
+		$res = wp_cache_get( $key );
+		if ( false === $res ) {
+			$filter = array(
+				'post_type'      => AmapressAdhesionRequest::INTERNAL_POST_TYPE,
+				'posts_per_page' => - 1,
+			);
+			if ( $status ) {
+				$filter['amapress_status'] = $status;
+			}
+			$res = array_map(
+				function ( $p ) {
+					return new AmapressAdhesionRequest( $p );
+				},
+				get_posts(
+					$filter
+				)
+			);
+			wp_cache_set( $key, $res );
+		}
+
+		return $res;
+	}
+
+	public function getEmail() {
+		return $this->getCustom( 'amapress_adhesion_request_email' );
+	}
+
+	public function getFirstName() {
+		return $this->getCustom( 'amapress_adhesion_request_first_name' );
+	}
+
+	public function getLastName() {
+		return $this->getCustom( 'amapress_adhesion_request_last_name' );
+	}
+
+	public function getAdresse() {
+		return $this->getCustom( 'amapress_adhesion_request_adresse' );
+	}
+
+	public function getTelephone() {
+		return $this->getCustom( 'amapress_adhesion_request_telephone' );
+	}
+
+	public function getOtherInfo() {
+		return $this->getCustom( 'amapress_adhesion_request_other_info' );
+	}
+
+	public function getIntermittent() {
+		return $this->getCustom( 'amapress_adhesion_request_intermittent' );
+	}
+
+	/** @return AmapressContrat_instance[] */
+	public function getContratInstances() {
+		return $this->getCustomAsEntityArray( 'amapress_adhesion_request_contrat_intances', 'AmapressContrat_instance' );
+	}
+
+	/** @return AmapressLieu_distribution[] */
+	public function getLieux() {
+		return $this->getCustomAsEntityArray( 'amapress_adhesion_request_lieux', 'AmapressLieu_distribution' );
+	}
+
+	/** @return AmapressContrat[] */
+	public function getContrats() {
+		return $this->getCustomAsEntityArray( 'amapress_adhesion_request_contrats', 'AmapressContrat' );
+	}
 }
--- entities/adhesion_request/register.php
+++ entities/adhesion_request/register.php
@@ -8,22 +8,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_filter( 'amapress_register_entities', 'amapress_register_entities_adhesion_request' );
 function amapress_register_entities_adhesion_request( $entities ) {
 	$entities['adhesion_request'] = array(
-		'internal_name'    => 'amps_adh_req',
-		'singular'         => amapress__( 'Demande de préinscription' ),
-		'plural'           => amapress__( 'Demandes de préinscription' ),
-		'public'           => 'adminonly',
-		'show_in_menu'     => false,
-		'show_in_nav_menu' => false,
-		'special_options'  => array(),
-		'slug'             => 'adhesions_requests',
-		'title_format'     => 'amapress_adhesion_request_title_formatter',
-		'slug_format'      => 'from_title',
-		'title'            => false,
-		'editor'           => false,
+		'internal_name'      => 'amps_adh_req',
+		'singular'           => amapress__( 'Demande de préinscription' ),
+		'plural'             => amapress__( 'Demandes de préinscription' ),
+		'public'             => 'adminonly',
+		'show_in_menu'       => false,
+		'show_in_nav_menu'   => false,
+		'special_options'    => array(),
+		'slug'               => 'adhesions_requests',
+		'title_format'       => 'amapress_adhesion_request_title_formatter',
+		'slug_format'        => 'from_title',
+		'title'              => false,
+		'editor'             => false,
 //        'menu_icon' => 'flaticon-business',
-		'redirect_archive' => 'amapress_redirect_agenda',
+		'redirect_archive'   => 'amapress_redirect_agenda',
 //        'menu_icon' => 'fa-menu fa-university',
-		'views'            => array(
+		'views'              => array(
 			'remove'  => array( 'mine' ),
 			'_dyn_'   => 'amapress_adhesion_request_views',
 			'exp_csv' => true,
@@ -32,11 +32,18 @@ function amapress_register_entities_adhesion_request( $entities ) {
 		'default_orderby'    => 'post_date',
 		'default_order'      => 'ASC',
 		'show_date_column'   => true,
-		'fields'           => array(
+		'fields'             => array(
+			'email'             => array(
+				'name'       => amapress__( 'Email' ),
+				'type'       => 'text',
+				'required'   => true,
+				'desc'       => 'Email',
+				'is_email'   => true,
+				'searchable' => true,
+			),
 			'first_name'        => array(
 				'name'       => amapress__( 'Prénom' ),
 				'type'       => 'text',
-				'time'       => true,
 				'required'   => true,
 				'desc'       => 'Prénom',
 				'searchable' => true,
@@ -44,7 +51,6 @@ function amapress_register_entities_adhesion_request( $entities ) {
 			'last_name'         => array(
 				'name'       => amapress__( 'Nom' ),
 				'type'       => 'text',
-				'time'       => true,
 				'required'   => true,
 				'desc'       => 'Nom',
 				'searchable' => true,
@@ -52,11 +58,8 @@ function amapress_register_entities_adhesion_request( $entities ) {
 			'adresse'           => array(
 				'name'       => amapress__( 'Adresse' ),
 				'type'       => 'textarea',
-				'time'       => true,
-				'required'   => true,
 				'desc'       => 'Nom',
 				'searchable' => true,
-//                'group' => 'Adresse',
 			),
 			'adresse_localized' => array(
 				'name'               => amapress__( 'Localisé' ),
@@ -64,17 +67,16 @@ function amapress_register_entities_adhesion_request( $entities ) {
 				'field_name_prefix'  => 'amapress_adhesion_request',
 				'use_as_field'       => false,
 				'address_field_name' => 'amapress_adhesion_request_adresse',
-//                'postal_code_field_name' => 'amapress_lieu_distribution_code_postal',
-//                'town_field_name' => 'amapress_lieu_distribution_ville',
-//                'group' => 'Adresse',
 			),
 			'telephone'         => array(
 				'name'       => amapress__( 'Téléphone' ),
 				'type'       => 'text',
-				'time'       => true,
-				'required'   => true,
 				'desc'       => 'Téléphone',
 				'searchable' => true,
+			),
+			'other_info'        => array(
+				'name' => amapress__( 'Autres informations' ),
+				'type' => 'editor',
 			),
 			'lieux'             => array(
 				'name'              => amapress__( 'Lieux de distribution' ),
@@ -89,8 +91,14 @@ function amapress_register_entities_adhesion_request( $entities ) {
 					'placeholder' => 'Tous les lieux',
 				),
 			),
-			'contrat_intances'  => array(
+			'contrats'          => array(
 				'name'      => amapress__( 'Contrats' ),
+				'type'      => 'multicheck-posts',
+				'post_type' => AmapressContrat::INTERNAL_POST_TYPE,
+				'desc'      => 'Contrats',
+			),
+			'contrat_intances'  => array(
+				'name'      => amapress__( 'Contrats spécifiques' ),
 				'type'      => 'multicheck-posts',
 				'post_type' => AmapressContrat_instance::INTERNAL_POST_TYPE,
 				'desc'      => 'Contrats',
