5a388fc61d5e27e9484bb97789922e64eeb62dc4
sharevb <sharevb@gmail.com>
2020-05-04 19:32:35 +0200

    feat(Créneaux visite): affichage d'un message d'avertissement contre les modifications si des amapiens sont déjà inscrits à des créneaux

--- entities/visite/register.php
+++ entities/visite/register.php
@@ -83,15 +83,25 @@ function amapress_register_entities_visite( $entities ) {
 					/** @var TitanFrameworkOption $option */
 					$visite = new AmapressVisite( $option->getPostID() );
 
-					return 'Configurer un ou plusieurs créneau(x) séparés par des | et de la forme : <strong>Heure Début-Heure Fin(nom du créneau)</strong>
+					$ret            = '';
+					$users_in_slots = count( $visite->getUserIdsWithAnySlot() );
+					if ( $users_in_slots > 0 ) {
+						$ret .= sprintf(
+							'<p><strong style="color: red">Attention : %d amapien(s) sont déjà inscrits. Modifier la configuration peut impacter l\'affectation de leurs créneaux</strong></p>',
+							$users_in_slots
+						);
+					}
+
+					$ret .= 'Configurer un ou plusieurs créneau(x) séparés par des | et de la forme : <strong>Heure Début-Heure Fin(nom du créneau)</strong>
 <br/>Exemple : 9h-12h(matin)|14h-17h(aprem)|9h-17h(journée)<br/>' .
-					       sprintf( 'Créneau(x) horaire(s) actuels (<strong>visite de %s à %s</strong>) : %s',
-						       date_i18n( 'H:i', $visite->getStartDateAndHour() ),
-						       date_i18n( 'H:i', $visite->getEndDateAndHour() ),
-						       $visite->getSlotsDescription()
-					       );// .
+					        sprintf( 'Créneau(x) horaire(s) actuels (<strong>visite de %s à %s</strong>) : %s',
+						        date_i18n( 'H:i', $visite->getStartDateAndHour() ),
+						        date_i18n( 'H:i', $visite->getEndDateAndHour() ),
+						        $visite->getSlotsDescription()
+					        );// .
 //					       '<br/>' .
 //					       Amapress::makeWikiLink( 'https://wiki.amapress.fr/admin/distribution' );
+					return $ret;
 				},
 				'group' => '2/ Horaires',
 			),
