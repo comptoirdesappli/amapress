b3932267d4fc022367e89202d6c280a5c53b96a3
sharevb <sharevb@gmail.com>
2020-07-29 07:46:13 +0200

    fix(Règlement inscriptions): row action "Marquer reçu" et "Marquer non reçu" conditionnels

--- entities/contrat_paiement/register.php
+++ entities/contrat_paiement/register.php
@@ -31,7 +31,7 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 			'_dyn_'   => 'amapress_paiements_views',
 			'exp_csv' => true,
 		),
-		'fields'       => array(
+		'fields'           => array(
 //            'user' => array(
 //                'name' => amapress__('Amapien'),
 //                'type' => 'select-users',
@@ -85,7 +85,7 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 				'searchable'   => true,
 				'show_column'  => false,
 			),
-			'contrat'  => array(
+			'contrat'       => array(
 				'name'   => amapress__( 'Contrat' ),
 				'type'   => 'custom',
 				'hidden' => true,
@@ -115,7 +115,7 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 //					}
 //				),
 			),
-			'lieu'     => array(
+			'lieu'          => array(
 				'name'       => amapress__( 'Lieu' ),
 				'type'       => 'custom',
 				'hidden'     => true,
@@ -145,7 +145,7 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 					}
 				),
 			),
-			'status'   => array(
+			'status'        => array(
 				'name'         => amapress__( 'Statut' ),
 				'type'         => 'select',
 				'options'      => array(
@@ -161,7 +161,7 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 					'placeholder' => 'Tous les statut',
 				),
 			),
-			'type'     => array(
+			'type'          => array(
 				'name'       => amapress__( 'Type' ),
 				'type'       => 'select',
 				'options'    => array(
@@ -179,7 +179,7 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 					'placeholder' => 'Tous les type',
 				),
 			),
-			'amount'   => array(
+			'amount'        => array(
 				'name'         => amapress__( 'Montant' ),
 				'type'         => 'float',
 				'unit'         => '€',
@@ -187,7 +187,7 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 				'desc'         => 'Montant du chèque/espèces/virement',
 				'csv_required' => true,
 			),
-			'numero'   => array(
+			'numero'        => array(
 				'name'         => amapress__( 'Numéro du chèque' ),
 				'type'         => 'text',
 				'required'     => false,
@@ -196,13 +196,13 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 				'csv_required' => true,
 				'searchable'   => true,
 			),
-			'banque'   => array(
+			'banque'        => array(
 				'name'       => amapress__( 'Banque' ),
 				'type'       => 'text',
 				'desc'       => 'Banque émettrice ou "Esp." pour des règlements en espèces ou "Vir." pour virement ou "Mon." pour des règlements en monnaie locale',
 				'searchable' => true,
 			),
-			'emetteur' => array(
+			'emetteur'      => array(
 				'name'       => amapress__( 'Emetteur' ),
 				'type'       => 'text',
 				'required'   => true,
@@ -218,11 +218,21 @@ function amapress_register_entities_contrat_paiement( $entities ) {
 //                'desc' => 'Répartitions',
 //            ),
 		),
-		'row_actions'  => array(
-			'mark_rcv'   => 'Marquer reçu',
-			'unmark_rcv' => 'Marquer Non reçu',
+		'row_actions'      => array(
+			'mark_rcv'   => [
+				'label'     => 'Marquer reçu',
+				'condition' => function ( $adh_id ) {
+					return AmapressAmapien_paiement::NOT_RECEIVED == AmapressAmapien_paiement::getBy( $adh_id )->getStatus();
+				},
+			],
+			'unmark_rcv' => [
+				'label'     => 'Marquer Non reçu',
+				'condition' => function ( $adh_id ) {
+					return AmapressAmapien_paiement::NOT_RECEIVED == AmapressAmapien_paiement::getBy( $adh_id )->getStatus();
+				},
+			],
 		),
-		'bulk_actions' => array(
+		'bulk_actions'     => array(
 			'amp_cnt_pmt_mark_recv' => array(
 				'label'    => 'Marquer reçu',
 				'messages' => array(
