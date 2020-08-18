6f6f21ce38558ca8d34f38708d229d2bb6d09acd
sharevb <sharevb@gmail.com>
2020-08-18 08:05:21 +0200

    fix(Assistant inscription/adhésion en ligne): check_adhesion_received bloquant dès l'étape coordonnées

--- entities/contrat_adhesion/shortcodes.php
+++ entities/contrat_adhesion/shortcodes.php
@@ -1028,33 +1028,19 @@ Vous pouvez configurer l\'email envoyé en fin de chaque inscription <a target="
 			}
 		}
 
-		if ( $user ) {
-			if ( isset( $_REQUEST['no_renew'] ) ) {
-				$reason = sanitize_textarea_field( isset( $_REQUEST['no_renew_reason'] ) ? $_REQUEST['no_renew_reason'] : '' );
-				update_user_meta( $user->ID, 'amapress_user_no_renew', 1 );
-				update_user_meta( $user->ID, 'amapress_user_no_renew_reason', $reason );
+		if ( ! $admin_mode && $user ) {
+			$adh_period = AmapressAdhesionPeriod::getCurrent( $adh_period_date );
+			if ( empty( $adh_period ) ) {
 				ob_clean();
 
-				$track_no_renews_email = $atts['track_no_renews_email'];
-				if ( empty( $track_no_renews_email ) ) {
-					$track_no_renews_email = get_option( 'admin_email' );
-				}
-				if ( ! empty( $track_no_renews_email ) ) {
-					$amapien   = AmapressUser::getBy( $user );
-					$edit_link = Amapress::makeLink( $amapien->getEditLink(), $amapien->getDisplayName() );
-					amapress_wp_mail(
-						$track_no_renews_email,
-						'Adhésion/Préinscription - Non renouvellement - ' . $amapien->getDisplayName(),
-						amapress_replace_mail_placeholders(
-							wpautop( "Bonjour,\n\nL\'amapien $edit_link ne souhaite pas renouveler. Motif:$reason\n\n%%site_name%%" ), $amapien ),
-						'', [], $notify_email
-					);
-				}
+				return ( sprintf( 'Aucune période d\'adhésion n\'est configurée au %s', date_i18n( 'd/m/Y', $adh_period_date ) ) );
+			}
 
-				return $additional_css . wp_unslash( amapress_replace_mail_placeholders( Amapress::getOption( 'online_norenew_message' ), null ) );
-			} else {
-				delete_user_meta( $user->ID, 'amapress_user_no_renew' );
-				delete_user_meta( $user->ID, 'amapress_user_no_renew_reason' );
+			$adh_paiement = AmapressAdhesion_paiement::getForUser( $user->ID, $adh_period_date, false );
+			if ( $check_adhesion_received && ( empty( $adh_paiement ) || $adh_paiement->isNotReceived() ) ) {
+				ob_clean();
+
+				return $additional_css . wp_unslash( Amapress::getOption( 'online_inscr_adhesion_required_message' ) );
 			}
 		}
 
