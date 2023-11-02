68cdb59d4f6092940f4414c91975a83bc35dd02c
sharevb <sharevb@gmail.com>
2023-11-02 22:13:33 +0100

    fix(Assistant d'inscription): si un contrat n'est distribué quand dans un lieu secondaire, alors le proposer à tous les adhérents

--- entities/contrat_adhesion/shortcodes.php
+++ entities/contrat_adhesion/shortcodes.php
@@ -2886,7 +2886,14 @@ Vous pouvez configurer l\'email envoyé en fin de chaque inscription <a target="
 						$before_close_hours = intval( $atts['before_close_hours'] );
 					}
 					if ( ! empty( $adh_paiement ) && ! empty( $adh_paiement->getLieuId() ) ) {
-						if ( ! in_array( $adh_paiement->getLieuId(), $contrat->getLieuxIds() ) ) {
+						$contrat_principal_lieux = array_filter(
+							$contrat->getLieux(),
+							function ( $l ) {
+								return $l->isPrincipal();
+							}
+						);
+						if ( ! empty( $contrat_principal_lieux )
+						     && ! in_array( $adh_paiement->getLieuId(), $contrat->getLieuxIds() ) ) {
 							return false;
 						}
 					}
